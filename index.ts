b3d6a747fba3af436af244fcff5f62baa27c2884
tiagoguatierri <44383573+tiagoguatierri@users.noreply.github.com>
2022-11-09 08:38:10 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -5,10 +5,12 @@ import { people, Person } from './data';
 const data$ = of<Person[]>(people);
 const initialData$ = data$.pipe(map((data) => data.slice(0, 5)));
 
+type Fn = <T>(...args:any[]) => unknown;
+
 function filterArray<T>(
-  query?: Record<string, unknown>,
-  inputMapFn?: (data: T[]) => any,
-  outputMapFn?: (data: T | T[], filtered: T[]) => any
+  query?: Record<string, unknown> | Fn,
+  inputMapFn?: Fn,
+  outputMapFn?: Fn
 ) {
   function handler(token: string) {
     const _handler = {
@@ -56,11 +58,14 @@ function filterArray<T>(
   return (source: Observable<T[]>) =>
     source.pipe(
       map((data) => {
-        const items = inputMapFn ? inputMapFn(data) : data;
-        const doFilter = items && query && !!Object.keys(query).length;
+        const items = (inputMapFn ? inputMapFn(data) : data) as T[];
+        const queryParams =
+          query && typeof query === 'function' ? query(data) : query;
+        const doFilter = items && !!Object.keys(queryParams).length;
+        console.log(queryParams)
         const filtered = doFilter
           ? items.filter((item: T) => {
-              for (const [key, value] of Object.entries<any>(query)) {
+              for (const [key, value] of Object.entries<any>(queryParams)) {
                 const [token, val] = parametrize(value);
                 return handler(token)(item, key, val);
               }
@@ -80,13 +85,13 @@ function write(ctx: any) {
   )}</pre>`;
 }
 
-const term = new BehaviorSubject<string>('Teste');
+const term = new BehaviorSubject<string>('^joa');
 
 combineLatest([term, initialData$])
   .pipe(
     filterArray(
-      { name: '^joao' },
-      ([params, items]) => items,
+      ([name]) => ({ name }),
+      ([, items]) => items,
       (data, filtered) => [data[0], filtered]
     )
   )
