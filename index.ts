e1b1000a4f2e924dea714d9426fb7f56f99e6b64
tiagoguatierri <44383573+tiagoguatierri@users.noreply.github.com>
2022-11-10 10:00:15 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -6,6 +6,9 @@ import {
   fromEvent,
   distinctUntilChanged,
   startWith,
+  merge,
+  concat,
+  race,
 } from 'rxjs';
 import { map, mergeMap, debounceTime } from 'rxjs/operators';
 import { people, Person } from './data';
@@ -15,8 +18,15 @@ const initialData$ = data$.pipe(map((data) => data.slice(0, 5)));
 
 type Fn = (...args: any[]) => any;
 
-function filterArray<T>(
-  query?: Record<keyof T, unknown> | Fn,
+/**
+ *
+ * @param query
+ * @param inputMapFn
+ * @param outputMapFn
+ * @returns
+ */
+export function filterArray<T>(
+  query?: Record<string, unknown> | Fn,
   inputMapFn?: Fn,
   outputMapFn?: Fn
 ) {
@@ -26,17 +36,18 @@ function filterArray<T>(
         const [item, key, val] = args;
         return eval(item[key] + token + val);
       },
-      '=': (...args: any[]) => {
-        const [item, key, val] = args;
-        return item[key] === val;
-      },
       '^': (...args: any[]) => {
+        const [item, key, val] = args;
+        return normalize(item[key]).startsWith(normalize(val));
+      },
+      '*': (...args: any[]) => {
         const [item, key, val] = args;
         return normalize(item[key]).includes(normalize(val));
       },
     };
 
     const key = token in _handler ? token : 'default';
+    console.log(key);
     return _handler[key];
   }
 
@@ -49,15 +60,16 @@ function filterArray<T>(
       : '';
   }
 
-  function parametrize(value: any, token = '=') {
-    value = value.toString();
-    const ctx = normalize(value).split(/[^a-z\d]/i);
-    const max = ctx.length - 1;
-    const val = [...ctx].pop();
-    return ctx.length > 1 ? [value.slice(0, max), val] : [token, val];
+  function parametrize(input: unknown) {
+    const value = input.toString();
+    const pattern = /^[\^|\*|>|<|>=|<=|=|!=]{1,2}/i;
+    const matcher = value.match(pattern);
+    const cleanValue = value.replace(pattern, '');
+    const token = matcher?.[0] || '==';
+    return [cleanValue, token];
   }
 
-  return (source: Observable<T[]>) =>
+  return (source: Observable<T>) =>
     source.pipe(
       map((data) => {
         const items = (inputMapFn ? inputMapFn(data) : data) as T[];
@@ -67,7 +79,8 @@ function filterArray<T>(
         const filtered = doFilter
           ? items.filter((item: T) => {
               for (const [key, value] of Object.entries<any>(queryParams)) {
-                const [token, val] = parametrize(value);
+                const [val, token] = parametrize(value);
+                //console.log(key, token, val);
                 return val ? handler(token)(item, key, val) : items;
               }
             })
@@ -88,20 +101,30 @@ function write(ctx: any) {
   )}</pre>`;
 }
 
-const input = fromEvent(document.querySelector('#inputSearch'), 'input').pipe(
-  distinctUntilChanged(),
-  debounceTime(500),
-  map((evt: InputEvent) => `^${evt.target['value']}`),
+const inputSearch = document.querySelector('#inputSearch') as HTMLInputElement;
+const inputToken = document.querySelector('#inputToken') as HTMLInputElement;
+
+const inputSearch$ = fromEvent(inputSearch, 'input').pipe(
+  map((evt: InputEvent) => evt.target['value']),
   startWith('')
 );
+const inputToken$ = fromEvent(inputToken, 'input').pipe(
+  map((evt: InputEvent) => evt.target['value']),
+  startWith('*')
+);
 
-combineLatest([input, data$])
+const search$ = combineLatest([inputSearch$, inputToken$]).pipe(
+  map(([val, token]) => token + val),
+  distinctUntilChanged(),
+  debounceTime(500)
+);
+
+combineLatest([search$, data$])
   .pipe(
     filterArray(
       ([name]) => ({ name }),
       ([, items]) => items,
       (data, filtered) => filtered
-    ),
-    mergeMap((items) => iif(() => !!items.length, of(items), data$))
+    )
   )
   .subscribe(write);
